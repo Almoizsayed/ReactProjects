01ae766ea35ba0cc021323b9b0da5b83f614738a
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-01 15:42:19 +0530

    Rendering List or Arrays in react

--- Learningreact/src/App.jsx
+++ Learningreact/src/App.jsx
@@ -1,16 +1,17 @@
 import reactLogo from "./assets/react.svg";
-import Hello from "./Components/Hello";
+import Fruit from "./Components/Fruit";
+// import Hello from "./Components/Hello";
 
 function App() {
-  const person = {
-    name: "Almoiz",
-    message: "You are doing Excellent",
-    seatnumber: [1, 9, 8],
-  };
+  // const person = {
+  //   name: "Almoiz",
+  //   message: "You are doing Excellent",
+  //   seatnumber: [1, 9, 8],
+  // };
 
   return (
     <div className="App">
-      <Hello person={person} />
+      <Fruit />
     </div>
   );
 }
---
+++ Learningreact/src/Components/Fruit.jsx
@@ -0,0 +1,9 @@
+function Fruit() {
+  const fruits = ["banana", "Mango", "grapes"];
+  return (
+    <div>
+      <h1> {fruits}</h1>
+    </div>
+  );
+}
+export default Fruit;
