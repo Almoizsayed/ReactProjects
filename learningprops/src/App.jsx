9000a483b85542ce2e8a3787a8a93d4b183c642b
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-01 11:17:20 +0530

    Propsfeature

---
+++ learningprops/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+
+import Hello from "./Components/Hello.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name="Almoiz" message="How are You!" />
+    </div>
+  );
+}
+
+export default App;
---
+++ learningprops/src/Components/Hello.jsx
@@ -0,0 +1,12 @@
+function Hello(props) {
+  return (
+    <div>
+      <h1>
+        {" "}
+        {props.name}
+        {props.message}
+      </h1>
+    </div>
+  );
+}
+export default Hello;
