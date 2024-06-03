8c44aa176806c172ee6e0f26615eb7e15afcae9f
Almoiz Sayyed <almoizs@heaptrace.net>
2024-06-03 16:44:10 +0530

    Handling Multiple Inputs Fields

--- Learningreact/src/Components/Form.jsx
+++ Learningreact/src/Components/Form.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, SetName] = useState("raj");
+  const [name, SetName] = useState({ firstName: "", lastName: "" });
   return (
     <div>
       <form>
         FullName:
-        <input type="text" onBlur={(e) => SetName(e.target.value)} />
-        <h1>{name}</h1>
+        <input
+          type="text"
+          onChange={(e) => SetName({ ...name, firstName: e.target.value })}
+          value={name.firstName}
+        />
+        <input
+          type="text"
+          onChange={(e) => SetName({ ...name, lastName: e.target.value })}
+          value={name.lastName}
+        />
+        <h1>{name.firstName}</h1>
+        <h1>{name.lastName}</h1>
       </form>
 
       {/* <h1>{{ name }}</h1> */}
